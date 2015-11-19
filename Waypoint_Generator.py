265842393138176fa7515da63f5734dc0f85f842
97xuzy <97xuzy@gmail.com>
2015-11-18 18:42:42 -0700

    Generate point in xy coordinates of a spiral

---
+++ Waypoint_Generator.py
@@ -0,0 +1,14 @@
+﻿import math
+
+def Sprial_Waypoint_Generator_xy( DistanceBetweenTurn, DistanceBetweenPoints):
+
+	a = DistanceBetweenTurn / math.pi
+	r = a * theta									# formula of  spiral
+	
+	for i in range(0, 100):
+		theta = i * (2 * math.pi / 100)
+		r = a * theta
+		x = r * math.cos(theta)						# polar to xy
+		y = r * math.sin(theta)						# polar to xy
+		print "("+str(x)+", "+str(y)+")\n"
+	
