fad16e6540decf6d45309973a38167618573c23f
Mark Omo <mwomo@email.arizona.edu>
2015-11-18 20:07:46 -0700

    Added better plotting and output

--- Waypoint_Generator.py
+++ Waypoint_Generator.py
@@ -1,14 +1,36 @@
 ﻿import math
+import LatLon
 
-def Sprial_Waypoint_Generator_xy( DistanceBetweenTurn, DistanceBetweenPoints):
+def Sprial_Waypoint_Generator_xy(home , DistanceBetweenTurn, NumPoints):
+    a = DistanceBetweenTurn / math.pi
+    #r = a * theta									# formula of  spiral
+    points = []
+    for i in range(0, NumPoints):
+        theta = i * (2 * math.pi / NumPoints)
+        r = a * theta
+        point = home.offset(math.degrees(theta), r/1000)
+        #x = r * math.cos(theta)						# polar to xy
+        #y = r * math.sin(theta)						# polar to xy
+        points.append(point)
+    return points
 
-	a = DistanceBetweenTurn / math.pi
-	r = a * theta									# formula of  spiral
-	
-	for i in range(0, 100):
-		theta = i * (2 * math.pi / 100)
-		r = a * theta
-		x = r * math.cos(theta)						# polar to xy
-		y = r * math.sin(theta)						# polar to xy
-		print "("+str(x)+", "+str(y)+")\n"
-	
+def Delm_to_latlon(delx,dely,clat,clon):
+    latx,lonx,num,zone = utm.from_latlon(clat, clon)
+    latx = latx+delx
+    lonx = lonx+dely
+    flat,flon = utm.to_latlon(latx,lonx,num,zone)
+    return [flat,flon]
+    
+
+if __name__ == "__main__":
+    home = LatLon.LatLon(LatLon.Latitude(32.2362917), LatLon.Longitude(-110.9501144))
+    points = Sprial_Waypoint_Generator_xy(home,50,100)
+    
+    mission = open('out.txt', 'w')
+    mission.write("QGC WPL 110\n0\t1\t0\t0\t0\t0\t0\t0\t0\t0\t0\t1\n")
+    for i, point in enumerate(points):
+        mission.write(str(i+1)+"1\t0\t3\t16\t0.000000\t0.000000\t0.000000\t0.000000\t")
+        mission.write(str(point.to_string("%D")[0])+"\t"+str(point.to_string("%D")[1]))
+        mission.write("\t100.000000\t1\n")
+        print point.to_string("%D")
+        #print math.degrees(latlon[0]),math.degrees(latlon[1])
